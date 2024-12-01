3dfc837312ec74c9dc561ef7165e9da5967cd745
Recep Enes Kaya <recepenskaya@gmail.com>
2024-12-01 16:57:40 +0300

    feat: add URLs API docs

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,21 +1,38 @@
-"""app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information, please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+"""
+
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # OpenAPI Schema Endpoint
+    # Serves the OpenAPI schema in JSON format.
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+
+    # Swagger UI
+    # Interactive API docs for testing endpoints in the browser.
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
+
+    # Redoc
+    # Alternative API documentation interface based on the OpenAPI schema.
+    path(
+        'api/redoc/',
+        SpectacularRedocView.as_view(url_name='api-schema'),
+        name='api-redoc',
+    ),
 ]
